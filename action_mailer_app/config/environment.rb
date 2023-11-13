e34e1d740f8bab4993f3e28e396dec05cb2a7f2d
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-11-13 16:06:23 +0900

    credentials.ymlの変更、SMTP

--- action_mailer_app/config/environment.rb
+++ action_mailer_app/config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.smtp_user_name,
+    password: Rails.application.credentials.smtp_password,
+    domain: 'perfect_rails@example.com',
+    address: 'smtp.mailgun.org',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+end
